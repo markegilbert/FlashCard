a2131008c6a50c02192115af942c02aeee920aa2
Mark Gilbert <5085540+markegilbert@users.noreply.github.com>
2024-11-21 14:16:50 -0500

    Got the question/answer toggling with a click

--- flashcard.ui/src/FlashCardList.jsx
+++ flashcard.ui/src/FlashCardList.jsx
@@ -9,21 +9,36 @@ const FlashCardList = () => {
             // TODO: This should come from the back-end
             const response = await fetch("https://localhost:7006/api/FlashCards?TopicID=MovieQuestions&NumberOfFlashcards=3");
             const rawFlashCards = await response.json();
+
+            // Add the showQuestion property to the array elements, defaulted to true so that the question shows up by default
+            const enrichedFlashCards = rawFlashCards.map(obj => ({ ...obj, showQuestion: true}));
+
             //setFlashCards([...flashcards,
             //                   rawFlashCards]);
-            setFlashCards(rawFlashCards);
+            setFlashCards(enrichedFlashCards);
         }
 
         fetchFlashCards();
     }, []);
 
 
+    const ToggleFlashCardSideVisibility = (fcIndex) => {
+        const updatedFlashCardList = [...flashcards];
+        updatedFlashCardList[fcIndex.index].showQuestion = !updatedFlashCardList[fcIndex.index].showQuestion;
+        setFlashCards(updatedFlashCardList);
+    }
+
     return (
         <>
             <link href="/css/FlashCard.css" rel="stylesheet" />
 
             <div id="FlashCardContainer" className="container">
-                {flashcards.map(fc => <p key={fc.id}>{fc.question}</p>) }
+                {flashcards.map((fc, index) => (
+                    <div className="flashCard" key={index} id={index} onClick={() => ToggleFlashCardSideVisibility({ index })}>
+                        <div className={fc.showQuestion ? 'flashCardQuestion' : 'flashCardQuestion flashCardHiddenSide' }>{fc.question}</div>
+                        <div className={fc.showQuestion ? 'flashCardAnswer flashCardHiddenSide' : 'flashCardAnswer'}>{fc.answer}</div>
+                    </div>
+                ))}
             </div>
         </>
     );
