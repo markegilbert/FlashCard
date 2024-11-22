79621447a374141118335ad5f5a82cbcfac741f3
Mark Gilbert <5085540+markegilbert@users.noreply.github.com>
2024-11-22 14:01:56 -0500

    Fixed a bug where the select control wasn't preserving the option selected

--- flashcard.ui/src/TopicSelection.jsx
+++ flashcard.ui/src/TopicSelection.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 const TopicSelection = (props) => {
     const [topics, setTopics] = useState([]);
     const [hasServiceError, setHasServiceError] = useState(false);
+    const [topicID, setTopicID] = useState("");
 
     useEffect(() => {
         const fetchTopics = async () => {
@@ -28,6 +29,12 @@ const TopicSelection = (props) => {
     }, []);
 
 
+    const handleChange = (selectedID) => {
+        props.onSelectTopic(selectedID);
+        setTopicID(selectedID);
+    };
+
+
     const TopicSelector = () => {
         if (hasServiceError) {
             return <p>There was a problem loading the list of available topics.  Please verify that the services are running.</p>;
@@ -38,8 +45,8 @@ const TopicSelection = (props) => {
         else {
             return (
                 <div className="sticky-div">
-                    <select id="TopicSelection" onChange={(e) => props.onSelectTopic(e.target.options[e.target.selectedIndex].id)}>
-                        {topics.map(t => <option key={t.id} id={t.id}>{t.topicName}</option>)}
+                    <select id="TopicSelection" onChange={(e) => handleChange(e.target.options[e.target.selectedIndex].id)} value={topicID}>
+                        {topics.map(t => <option key={t.id} id={t.id} value={t.id}>{t.topicName}</option>)}
                     </select>
                 </div>
             );
