12907e8c9f2f0e0062ff574b1dc3ac8913d01e35
Mark Gilbert <5085540+markegilbert@users.noreply.github.com>
2025-01-29 09:38:43 -0500

    Removed a console.log statement no longer needed

--- flashcard.ui/src/FlashCardList.jsx
+++ flashcard.ui/src/FlashCardList.jsx
@@ -56,7 +56,6 @@ const FlashCardList = (props) => {
 
         const handleScroll = () => {
             const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
-            console.log("handleScroll: scrollTop=" + scrollTop + "; clientHeight=" + clientHeight + "; scrollHeight=" + scrollHeight);
             if (scrollTop + clientHeight >= scrollHeight - 100) {
                 loadMoreForThisTopic();
             }
