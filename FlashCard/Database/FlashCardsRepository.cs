561bd4980ecaa7c2bab48953dbba6ea008c3c78e
Mark Gilbert <5085540+markegilbert@users.noreply.github.com>
2025-02-10 14:02:55 -0500

    Added API endpoints for inserting and deleting flashcards.

--- FlashCard/Database/FlashCardsRepository.cs
+++ FlashCard/Database/FlashCardsRepository.cs
@@ -5,8 +5,8 @@ namespace FlashCard.Database
 {
     public class FlashCardsRepository
     {
-
         private FlashCardDBContext _Context;
+
 
         public FlashCardsRepository(FlashCardDBContext Context)
         {
@@ -15,7 +15,6 @@ namespace FlashCard.Database
             this._Context = Context;
         }
 
-
         public async Task<ICollection<FlashCardModel>> GetRandomFlashCardsByTopic(String TopicID, int NumberOfFlashcards)
         {
             return (from f in await this._Context.FlashCards.ToListAsync() orderby Guid.NewGuid() select f)
@@ -23,5 +22,28 @@ namespace FlashCard.Database
                         .Take(NumberOfFlashcards)
                         .ToList();
         }
+
+
+        public void InsertFlashCard(FlashCardModel NewFlashCard)
+        {
+            this._Context.FlashCards.Add(NewFlashCard);
+        }
+
+
+        public async Task DeleteFlashCard(String PartitionKey, String FlashCardID)
+        {
+            FlashCardModel? FlashCardToRemove;
+
+            FlashCardToRemove = await this._Context.FlashCards.FindAsync(FlashCardID, PartitionKey);
+            if (FlashCardToRemove != null) { this._Context.FlashCards.Remove(FlashCardToRemove); }
+        }
+
+
+
+        public async Task<int> Save()
+        {
+            return await this._Context.SaveChangesAsync();
+        }
+
     }
 }
